3896e44ecbba0420bf3af52ce3220fbd9355a2b4
Lawrence McDaniel <lpm0073@gmail.com>
2018-03-03 13:36:40 -0600

    added base functionality to gulp

--- README.md
+++ README.md
@@ -8,5 +8,8 @@ npm install --save-dev gulp-util
 npm install --save-dev babel-core babel-register babel-preset-es2015
 npm install --save-dev gulp@next gulp-babel gulp-jshint gulp-sass gulp-sourcemaps gulp-less gulp-concat gulp-uglify gulp-rename gulp-clean-css del
 
+npm install --save-dev gulp-path
+npm install --save-dev gulp-copy
+
 touch gulpfile.babel.js
 ```
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,9 +9,14 @@ import del from 'del';
 
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
+import gulpPath from 'gulp-path';
 
 
 const paths = {
+  images: {
+    src: 'src/assets/img/**/*.*',
+    dest: 'www/assets/img/'
+  },
   styles: {
     src: 'src/styles/less/**/*.less',
     dest: 'www/assets/css/'
@@ -19,9 +24,23 @@ const paths = {
   scripts: {
     src: 'src/scripts/js/**/*.js',
     dest: 'www/assets/js/'
+  },
+  pages: {
+    src: 'src/pages/**/*.*',
+    dest: 'www/pages/'
+  },
+  shared: {
+    src: 'src/shared/**/*.*',
+    dest: 'www/shared/'
   }
 };
 
+
+/*
+ * Gulp Path
+ */
+var app = new gulpPath.Path('src', 'www');
+var stylePaths = app.generateAllPaths('stylePaths', 'css', null, 'sass');
 
 /*
  * For small tasks you can export arrow functions
@@ -31,6 +50,11 @@ export const clean = () => del([ 'assets' ]);
 /*
  * You can also declare named functions and export them as tasks
  */
+ export function images() {
+   return gulp.src(paths.images.src)
+     .pipe(gulp.dest(paths.images.dest));
+ }
+
 export function styles() {
   return gulp.src(paths.styles.src)
     .pipe(less())
@@ -51,12 +75,25 @@ export function scripts() {
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
+export function pages() {
+  return gulp.src(paths.pages.src)
+    .pipe(gulp.dest(paths.pages.dest));
+}
+
+export function shared() {
+  return gulp.src(paths.shared.src)
+    .pipe(gulp.dest(paths.shared.dest));
+}
+
  /*
   * You could even use `export as` to rename exported tasks
   */
 function watchFiles() {
+  gulp.watch(paths.images.src, images);
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.pages.src, pages);
+  gulp.watch(paths.shared.src, shared);
 }
 export { watchFiles as watch };
 
@@ -64,7 +101,7 @@ export { watchFiles as watch };
  * You can still use `gulp.task`
  * for example to set task names that would otherwise be invalid
  */
-const build = gulp.series(clean, gulp.parallel(styles, scripts));
+const build = gulp.series(clean, gulp.parallel(images, styles, scripts, pages, shared));
 gulp.task('build', build);
 
 /*
